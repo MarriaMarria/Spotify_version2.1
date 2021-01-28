43de7b3673e4c26aceb19a0cc2038901cb696551
MarriaMarria <maria.kononevs@gmail.com>
2021-01-28 22:25:02 +0100

    added buttons to decrease/increase sound and mute/unmute

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 let audios = ['./Media/forever.mp3', './Media/goodbye.mp3'];
-let covers = ['lplost.jpeg', 'mann.jpeg', 'meds.jpeg', 'serggate.jpeg'];
-
-// Object of audio
-
-///let audio = new Audio();
+// let covers = ['lplost.jpeg', 'mann.jpeg', 'meds.jpeg', 'serggate.jpeg'];
 
 
-window.onload = playSong; // Song plays when the window is loaded
+let currentSong = 0;
 
-function playSong() {
-    let currentSong = 0;
-    let audio_btns = document.querySelector(".audio-btns");
-    let audio = document.createElement('audio');
-    audio.src = audios[currentSong];
-    audio_btns.appendChild(audio);
-    audio.autoplay = true
-    audio.play();
-}
+let audio_btns = document.querySelector(".audio-btns");
+let audio = document.createElement('audio');
+audio.src = audios[currentSong];
+let playBtn = document.querySelector(".play-pause");
 
 function togglePlayPause() {
+    
     if(audio.paused) {
-        audio.play();
-        let playBtn = document.querySelector(".play-pause");
-        playBtn.addEventListener('click', () => {
+            audio.play();
             playBtn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
-        })    
     } else {
-        audio.pause();
-        playBtn.addEventListener('click', () => {
-        playBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-    })
+            audio.pause();
+            playBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+    }
+}    
+
+playBtn.addEventListener("click", togglePlayPause);
+
+let decreaseBtn = document.querySelector('.decrease');
+decreaseBtn.addEventListener('click', decreaseVolume);
+let increaseBtn = document.querySelector('.increase');
+increaseBtn.addEventListener('click', increaseVolume);
+
+
+function decreaseVolume() {
+    audio.volume -= 0.25;
 }
+
+function increaseVolume() {
+    audio.volume += 0.25;
 }
+
+
+let muteBtn = document.querySelector('.mute');
+muteBtn.addEventListener('click', mute);
+
+function mute() {
+    if (audio.muted == false) {
+    muteBtn.muted = true;
+    audio.muted = true;
+    } else {
+    audio.muted = false; 
+    muteBtn.muted = false;
+    }
+}
+
+
 
