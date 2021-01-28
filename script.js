136d4af7acebf87d04d99303cd6b3f3a0b4475cb
MarriaMarria <maria.kononevs@gmail.com>
2021-01-28 09:23:28 +0100

    first commit of the project mini Spotify

---
+++ script.js
@@ -0,0 +1,35 @@
+let audios = ['./Media/LP_Lost.mp3', './Media/Mann.mp3'];
+let covers = ['lplost.jpeg', 'mann.jpeg', 'meds.jpeg', 'serggate.jpeg'];
+
+// Object of audio
+
+///let audio = new Audio();
+
+
+window.onload = playSong; // Song plays when the window is loaded
+
+function playSong() {
+    let currentSong = 0;
+    let audio_btns = document.querySelector(".audio-btns");
+    let audio = document.createElement('audio');
+    audio.src = audios[currentSong];
+    audio_btns.appendChild(audio);
+    audio.autoplay = true
+    audio.play();
+}
+
+function togglePlayPause() {
+    if(audio.paused) {
+        audio.play();
+        let playBtn = document.querySelector(".play-pause");
+        playBtn.addEventListener('click', () => {
+            playBtn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+        })    
+    } else {
+        audio.pause();
+        playBtn.addEventListener('click', () => {
+        playBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+    })
+}
+}
+
